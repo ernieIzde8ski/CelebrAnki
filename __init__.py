2dbc105b288b318a14eb16a30e8ee0544563e73c
Ernest Izdebski <eizdeb2@wgu.edu>
2024-10-13 02:44:44 -0400

    implement choosing sound from random selection
    Added `.gitignore`. This keeps files like `__pycache__` from ever being
    staged, let alone committed.
    
    Added `requirements-dev.txt`. This allows a new dev to quickly install
    dev dependencies by running `pip install -r requirements-dev.txt`, which
    enables linter/autocomplete integrations in your favorite text editor.
    
    Removed dev-machine-specific information from `meta.json`. Source code
    shouldn't have user configs in it; see how Ankimon does it for
    reference:
    
      <https://github.com/Unlucky-Life/ankimon/blob/main/src/Ankimon/meta.json>
    
    (If you do want to have it update on your machine, see [this
    StackOverflow answer](https://stackoverflow.com/a/40272289))
    
    Added some basic type hints to the source code, using Python 3.9 syntax.
    This gets rid of some basic type errors. Because of the 3.9 requirement,
    this commit requires Anki to be at least version 2.1.50+ (released
    2022-04-09).
    
    Revised the `sound_file` string into a `sound_files` list. For backwards
    compatibility, its value in the config file can be either a string, a
    literal `0`, or a list of strings.
    
    Closes #1.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,33 @@
 import os
+import random
+from typing import TYPE_CHECKING, Any, Literal, Union, cast
+
+from anki.sound import AVTag
+from aqt import gui_hooks, mw, qconnect
+from aqt.sound import AVPlayer, clearAudioQueue, play
+from aqt.webview import AnkiWebView, AnkiWebViewKind
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QFileDialog
-from aqt import gui_hooks, qconnect
-from aqt import mw
-from aqt.sound import AVPlayer, play, clearAudioQueue
-from aqt.webview import AnkiWebView, AnkiWebViewKind
 
-addon_path = os.path.dirname(__file__)
-user_files = os.path.join(addon_path, "user_files")
-config = mw.addonManager.getConfig(__name__)
+if TYPE_CHECKING:
+    assert mw is not None
 
-if config["sound"]:
-    sound_file = config["sound"]
+addon_path: str = os.path.dirname(__file__)
+user_files: str = os.path.join(addon_path, "user_files")
+config = cast(dict[str, Any], mw.addonManager.getConfig(__name__))
+
+sound: Union[str, list[str], Literal[0]] = config["sound"]
+sound_files: list[str]
+
+if sound and isinstance(sound, list):
+    sound_files = config["sound"]
+elif sound:  # `sound` must be a string
+    sound_files = [config["sound"]]
+    setting = {"sound": sound_files}
+    mw.addonManager.writeConfig(__name__, setting)
 else:
-    sound_file = os.path.join(user_files, "nyt.mp3")
-    setting = {"sound": sound_file}
+    sound_files = [os.path.join(user_files, "nyt.mp3")]
+    setting = {"sound": sound_files}
     mw.addonManager.writeConfig(__name__, setting)
 
 
@@ -22,35 +35,35 @@ def play_sound(webview: AnkiWebView):
     if webview.kind != AnkiWebViewKind.MAIN:
         return
     clearAudioQueue()
-    mw.progress.single_shot(
-        1, lambda: play(sound_file), False
-    )
+    sound_file = random.choice(sound_files)
+    mw.progress.single_shot(1, lambda: play(sound_file), False)
 
 
 gui_hooks.webview_did_inject_style_into_page.append(play_sound)
 
 
-def _play_tags(self, tags):
+def _play_tags(self: AVPlayer, tags: list[AVTag]):
     self._enqueued = tags[:]
-    if self.interrupt_current_audio and False:
-        self._stop_if_playing()
     self._play_next_if_idle()
 
 
 AVPlayer.play_tags = _play_tags
 
 
-def updateSound():
-    global sound_file, config
-    file_name = QFileDialog.getOpenFileName(mw, "Change sound", user_files, "Sound Files (*.wav *.mp3 *.mp4)")
-    if file_name != sound_file:
-        sound_file = file_name[0]
-        setting = {"sound": sound_file}
+def update_sound():
+    global sound_files, config
+    (file_names, _) = QFileDialog.getOpenFileNames(
+        mw, "Change sound", user_files, "Sound Files (*.wav *.mp3 *.mp4)"
+    )
+    file_names.sort()
+    if file_names != sound_files:
+        sound_files = file_names
+        setting = {"sound": sound_files}
         mw.addonManager.writeConfig(__name__, setting)
         gui_hooks.webview_did_inject_style_into_page.remove(play_sound)
         gui_hooks.webview_did_inject_style_into_page.append(play_sound)
 
 
 action = QAction("Change completion jingle")
-qconnect(action.triggered, updateSound)
+qconnect(action.triggered, update_sound)
 mw.form.menuTools.addAction(action)
